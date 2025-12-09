b325d06ff14dc2e871a38f3f0b804c95e77d9e9c
Artur Kasimov <isysartur@yandex.ru>
2025-12-09 15:17:54 +0300

    fix: align cypress baseUrl and lint order-info

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:4000',
     specPattern: 'cypress/e2e/**/*.cy.{ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
     video: false
   }
 });
+
 
